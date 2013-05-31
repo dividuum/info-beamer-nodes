d6286b85b98a29649a8f24c7d6362513768e01ab
Florian Wesch <fw@dividuum.de>
2013-05-31 11:10:33 +0200

    video playlist example

---
+++ videolist/README.txt
@@ -0,0 +1,5 @@
+Simple Video Playlist
+
+Put the filenames of your videos in playlist.txt. One video file name per line. 
+Filenames are case sensitive. The playlist can be changed during runtime by
+updating the file playlist.txt and saving it.
---
+++ videolist/node.lua
@@ -0,0 +1,29 @@
+gl.setup(1920, 1080)
+
+local playlist
+local current_video_idx
+
+util.file_watch("playlist.txt", function(content) 
+    playlist = {}
+    for filename in string.gmatch(content, "[^\r\n]+") do
+        playlist[#playlist+1] = filename
+    end
+    current_video_idx = 0
+    print("new playlist")
+    pp(playlist)
+end)
+
+function next_video()
+    current_video_idx = current_video_idx + 1
+    if current_video_idx > #playlist then
+        current_video_idx = 1
+    end
+    video = util.videoplayer(playlist[current_video_idx], {loop=false})
+end
+
+function node.render()
+    if not video or not video:next() then
+        next_video()
+    end
+    util.draw_correct(video, 0, 0, WIDTH, HEIGHT)
+end
