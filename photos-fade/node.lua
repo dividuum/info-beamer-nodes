734ea08f38bd8c3995048d0916ec04c303bf9889
Florian Wesch <fw@dividuum.de>
2013-06-27 20:25:28 +0200

    Added delay between loading and switching of images.
    On the pi, jpg loading can be quiet load. Since sys.now() always
    returns the same value when called within one frame, the time
    when the image is finished loading is unknown.
    
    We therefore include a 3 seconds safe buffer.

--- photos-fade/node.lua
+++ photos-fade/node.lua
@@ -1,5 +1,10 @@
 local INTERVAL = 10
 
+local SWITCH_DELAY = 3 -- enough time to load next image
+
+-- use 'export INFOBEAMER_USE_HW_JPEG=1' if you are using the
+-- raspberry pi version for faster jpeg loading
+
 gl.setup(1920, 1080)
 
 pictures = util.generator(function()
@@ -15,7 +20,7 @@ node.event("content_remove", pictures.remove)
 
 util.set_interval(INTERVAL, function()
     local next_image_name = pictures.next()
-    print("now showing " .. next_image_name)
+    print("now loading" .. next_image_name)
     last_image = current_image
     current_image = resource.load_image(next_image_name)
     fade_start = sys.now()
@@ -23,8 +28,10 @@ end)
 
 function node.render()
     gl.clear(0,0,0,1)
-    local delta = sys.now() - fade_start
-    if delta < 1 and last_image then
+    local delta = sys.now() - fade_start - SWITCH_DELAY
+    if last_image and delta < 0 then
+        util.draw_correct(last_image, 0, 0, WIDTH, HEIGHT)
+    elseif last_image and delta < 1 then
         util.draw_correct(last_image, 0, 0, WIDTH, HEIGHT, 1 - delta)
         util.draw_correct(current_image, 0, 0, WIDTH, HEIGHT, delta)
     else
